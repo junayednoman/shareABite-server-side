49ffa82fde869e692f7e42d423be3bb3b50b253a
junayednoman <junayednoman05@gmail.com>
2023-11-06 11:07:23 +0600

    server setup and get api setup done for food collection

---
+++ index.js
@@ -0,0 +1,61 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const app = express();
+const port = 5000;
+
+// middlewares
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send('This server is running')
+})
+
+
+// ========================== ********** MONGO DB CODE START ******** =======================
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r8yk5up.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const foodCollection = client.db("shareABite").collection("foods");
+
+        // apis for foods
+        app.get('/foods', async (req, res) => {
+            const cursor = foodCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+            
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+// ========================== **** MONGO DB CODE END ***** =======================
+
+
+app.listen(port, () => {
+    console.log('server running from port: ', port)
+})
